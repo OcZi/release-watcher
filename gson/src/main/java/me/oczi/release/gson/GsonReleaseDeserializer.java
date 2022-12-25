69b650b60dd13c71ae9bf8e61284ba8c849bcfa7
salva <atlas200x@gmail.com>
2022-12-25 06:28:53 -0500

    refactor: remove unused imports

--- gson/src/main/java/me/oczi/release/gson/GsonReleaseDeserializer.java
+++ gson/src/main/java/me/oczi/release/gson/GsonReleaseDeserializer.java
@@ -1,11 +1,9 @@
 package me.oczi.release.gson;
 
 import com.google.gson.*;
-import me.oczi.release.GithubReleases;
 import me.oczi.release.Release;
 
 import java.lang.reflect.Type;
-import java.util.Arrays;
 
 /**
  * {@link Release} Deserializer for github API.
